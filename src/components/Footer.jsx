30647c664ca3c659f840e1be2388d6430b63d573
samanderson2003 <sam.youngren2003@gmail.com>
2026-02-27 19:01:11 +0530

    feat: updated color and removed unwanted contents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,28 +7,8 @@ const Footer = () => {
             <div className="container">
                 <div className="footer-top">
                     <div className="footer-brand">
-                        <h2 className="footer-logo">P I X N X T</h2>
+                        <img src="/Logo_Final-01.png" alt="Pixnxt Logo" className="footer-logo-img" />
                         <p className="footer-tagline">Designed for photographers. Built to help you grow.</p>
-                    </div>
-                    <div className="footer-links">
-                        <div className="link-group">
-                            <h4>Products</h4>
-                            <ul>
-                                <li><a href="#">Client Gallery</a></li>
-                                <li><a href="#">Website</a></li>
-                                <li><a href="#">Studio Manager</a></li>
-                                <li><a href="#">Store</a></li>
-                            </ul>
-                        </div>
-                        <div className="link-group">
-                            <h4>Resources</h4>
-                            <ul>
-                                <li><a href="#">Examples</a></li>
-                                <li><a href="#">Pricing</a></li>
-                                <li><a href="#">Blog</a></li>
-                                <li><a href="#">Help Center</a></li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
                 <div className="footer-bottom">
